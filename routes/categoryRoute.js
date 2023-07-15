64b318154e49eff67ec5ecaf102d0814bdd0072c
Ahmedashraftaha <ahmedashraft7@gmail.com>
2023-07-16 01:33:33 +0300

    "crud operation category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
-const { getCategories,creatCategory} = require('../services/categoryServices');
+const { getCategories,creatCategory,getCategory,updateCategory,deleteCategory} = require('../services/categoryServices');
 
 const router = express.Router();
 
-router.route('/').get(getCategories).post(creatCategory)
-
-
+router.route('/').get(getCategories).post(creatCategory);
+router.route('/:id').get(getCategory).put(updateCategory).delete(deleteCategory);
 
 
 module.exports = router;
--- services/categoryServices.js
+++ services/categoryServices.js
@@ -1,22 +1,56 @@
 const CategoryModel = require('../models/categoryModel');
 const slugify = require('slugify')
-exports.getCategories = (req, res) => {
-//   const name = req.body.name;
-//   console.log(req.body);
+const asyncHandler = require('express-async-handler')
+exports.getCategories = asyncHandler(async(req, res) => {
+    const page=req.query.page  * 1 || 1;
+    const limit =req.query.limit  * 1 || 5;
+    const skip=(page-1)*limit;
+ const categories= await  CategoryModel.find({}).skip(skip).limit(limit);
+ res.status(200).json({result:categories.length ,page, data :categories})
 
-//   const newCategory = new CategoryModel({ name });
-//   newCategory
-//     .save()
-//     .then((doc) => {
-//       res.json(doc);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-res.send();
-};
+});
 
-exports.creatCategory=(req,res)=>{
-    const name = req.body.name;
-    CategoryModel.create({name,slug:slugify(name)}).then(category=>res.status(200).json({data:category})).catch(err=>res.status(400).send(err))
+exports.getCategory= asyncHandler(async(req,res)=>{
+    const {id}= req.params;
+    const category= await CategoryModel.findById(id);
+    
+    if(!category){
+        res.status(404).json({msg : "No Category found"});
+    }else
+{
+    res.status(200).json({data : category});
 }
+})
+
+exports.creatCategory=asyncHandler(async(req,res)=>{
+    const name = req.body.name;
+    const category= await CategoryModel.create({name,slug:slugify(name)})
+    res.status(200).json({data:category})
+})
+
+exports.updateCategory = asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    const name =req.body.name;
+
+    const category = await CategoryModel.findOneAndUpdate(
+        { _id: id },
+        { name, slug: slugify(name) },
+        { new: true }
+      );
+
+    if(!category){
+        res.status(404).json({msg : "No Category found"});
+    }else
+{
+    res.status(200).json({data : category});
+}
+})
+exports.deleteCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const category = await CategoryModel.findByIdAndDelete(id);
+  
+    if (!category) {
+      res.status(404).json({ msg: `No category for this id ${id}` });
+    }
+    res.status(204).send();
+  });
